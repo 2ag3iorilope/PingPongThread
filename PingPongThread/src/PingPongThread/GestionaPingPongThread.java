69931f87229ef267d32bc97d3844bdc1077b874c
ioritzlope45@gmail.com <ioritzlope45@gmail.com>
2024-11-25 09:16:12 +0100

    A2 aldaketak eginda

--- PingPongThread/src/PingPongThread/GestionaPingPongThread.java
+++ PingPongThread/src/PingPongThread/GestionaPingPongThread.java
@@ -5,9 +5,12 @@ public class GestionaPingPongThread {
 
 		PingPongThread r1 = new PingPongThread("PING", 33); // Ping Esaldia 33 aldiz
 		PingPongThread r2 = new PingPongThread("PONG", 10); // Pong Esaldia 10 aldiz
+		
+		  Thread hilo1 = new Thread(r1, "hiloPI");
+	        Thread hilo2 = new Thread(r2, "hiloPO");
 
-		r1.start();
-		r2.start();
+	        hilo1.start();
+	        hilo2.start();
 	}
 }
 //Programa honek bi hari sortzen ditu, eta hainbat esaldi inprimatzen dituzte ("PING" eta "PONG"), hainbat aldiz. 
--- PingPongThread/src/PingPongThread/PingPongThread.java
+++ PingPongThread/src/PingPongThread/PingPongThread.java
@@ -1,21 +1,28 @@
 package PingPongThread;
 
-public class PingPongThread extends Thread {
-	private String frase;
-	private int veces;
+public class PingPongThread implements Runnable {
+    private String frase; //Erakutsiko den esaldia
+    private int veces;   //Kopurua
 
-	public PingPongThread(String queDecir, int cuantasVeces) {
-		frase = queDecir; // Erakutsiko den esaldia
-		veces = cuantasVeces; // Kopurua
-	}
+    /**
+     * Eraikitzailea
+     * @param queDecir , erakutsi behar den esaldia
+     * @param cuantasVeces , erakutsiko den kopurua
+     */
+    public PingPongThread(String queDecir, int cuantasVeces) {
+        this.frase = queDecir;
+        this.veces = cuantasVeces;
+    }
 
-	/**
-	 * Hariaren portaera definitzen duen metodoa. Inprimatu esaldia zehaztutako aldi
-	 * kopurua, eta, ondoren, haria amaitu dela adierazten du.
-	 */
-	public void run() {
-		for (int i = 1; i <= veces; i++)
-			System.out.print(frase + " ");
-		System.out.println("Ha finalizado la ejecución del hilo: " + getName());
-	}
+    /**
+     *Hariaren portaera definitzen duen metodoa. Inprimatu esaldia
+     * zehaztutako aldi kopurua, eta, ondoren, haria amaitu dela adierazten du.
+     */
+    @Override
+    public void run() {
+        for (int i = 1; i <= veces; i++) {
+            System.out.print(frase + " ");
+        }
+        System.out.println("Ha finalizado la ejecución del hilo: " + Thread.currentThread().getName());
+    }
 }
